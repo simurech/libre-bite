987755806b0cbd339ae041bd164d0c40254e4be7
Simon Urech <simurech@gmail.com>
2026-03-17 13:44:28 +0100

    Author-URL korrigiert

--- libre-bite.php
+++ libre-bite.php
@@ -7,7 +7,7 @@
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Simon Urech
- * Author URI:        https://github.com/simurech/libre-bite
+ * Author URI:        https://urech.dev/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       libre-bite
